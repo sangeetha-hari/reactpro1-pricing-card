4717356e236c160e588b8444f6e1487528cd6230
sangeetha-hari <harikantrasangeetha4@gmail.com>
2022-06-30 19:00:07 +0530

    this is my-app1 first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PrizeCard from './PrizeCard'
+
+export default function App(){
+  return(
+    <div>
+      Welcom to guvi
+      <PrizeCard />
+    </div>
+  )
+}
